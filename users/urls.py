fe16b4eb8e7e91242239dee6325a5c1e619b228b
Aziz Ouadoud <aziz.ouadoud@etu.uae.ac.ma>
2022-03-16 19:45:14 +0100

    This is a new commit for what I originally planned to be amended

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from .views import CustomUserCreate, BlacklistTokenView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('create/', CustomUserCreate.as_view(), name="create_user"),
+    path('logout/blacklist/', BlacklistTokenView.as_view(), name="blacklist"),
+]
